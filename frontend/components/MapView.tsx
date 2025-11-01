06afc03443ce830d7975d22180609e94296f25b1
Bader123457 <ibadr2005@gmail.com>
2025-11-01 17:51:36 +0000

    got an api key for frontend 2

--- frontend/components/MapView.tsx
+++ frontend/components/MapView.tsx
@@ -30,10 +30,12 @@ export default function MapView() {
           zoom={12}
           style={{ height: "100%", width: "100%" }}
         >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+         <TileLayer
+  attribution='&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  url={`https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=${process.env.NEXT_PUBLIC_MAPTILER_KEY}`}
+/>
+
+        
           <FeatureGroup>
             <EditControl
               position="topright"
